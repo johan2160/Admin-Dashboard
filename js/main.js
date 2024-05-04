5486fbd6529d2bceb695a7173df6b22f639290b1
johandev21 <johan.p.2160@gmail.com>
2024-05-03 22:39:33 -0400

    feat: Implement mobile menu toggle functionality
    This commit adds functionality to display and hide the aside menu for mobile devices. Users can now toggle the menu visibility by clicking on the menu icon.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 const themeToggler = document.querySelector("#theme-toggler");
+const displayMenu = document.querySelector("#display-menu")
+const hideMenu = document.querySelector("#hide-menu")
+const aside = document.querySelector("aside")
 
+// Toggling between themes
 themeToggler.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme-variables');
 })
+
+// Prevent aside menu not showing on screen larger than phones
+function isMobileScreen() {
+  return window.innerWidth <= 765; // You can adjust this threshold according to your design needs
+}
+
+function updateAsideDisplay() {
+  if (isMobileScreen()) {
+    aside.style.display = "none";
+  } else {
+    aside.style.display = "flex";
+  }
+}
+
+// Initial check
+updateAsideDisplay();
+
+window.addEventListener('resize', updateAsideDisplay);
+
+// Display/hide aside menu
+displayMenu.addEventListener('click', () => {
+  aside.style.display = "block";
+});
+
+hideMenu.addEventListener('click', () => {
+  aside.style.display = "none";
+});
