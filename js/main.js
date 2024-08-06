2b3fc8924974ee89c0a5a993456d6fc0e853c169
johandev21 <johan.p.2160@gmail.com>
2024-08-05 21:37:07 -0400

    feat: enhance mobile menu functionality and theme toggler
    - Refactored mobile menu logic to handle display based on screen width
    - Updated event listeners for display and hide actions to animate menu sliding
    - Ensured aside menu is positioned correctly on larger screens
    - Maintained theme toggling functionality for dark mode

--- js/main.js
+++ js/main.js
@@ -1,36 +1,27 @@
 const themeToggler = document.querySelector("#theme-toggler");
 const displayMenu = document.querySelector("#display-menu")
 const hideMenu = document.querySelector("#hide-menu")
-const aside = document.querySelector("aside")
+const aside = document.querySelector("aside");
 
 // Toggling between themes
 themeToggler.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme-variables');
 })
 
-// Prevent aside menu not showing on screen larger than phones
-function isMobileScreen() {
-  return window.innerWidth <= 765; // You can adjust this threshold according to your design needs
-}
-
-function updateAsideDisplay() {
-  if (isMobileScreen()) {
-    aside.style.display = "none";
+function mobileMenu() {
+  if (window.innerWidth <= 765) {
+    displayMenu.addEventListener('click', () => {
+      aside.style.left = "0";
+    })
+    
+    hideMenu.addEventListener('click', () => { 
+      aside.style.left = "-100%";
+    })
   } else {
-    aside.style.display = "flex";
-  }
+    aside.style.left = "0";
+  }  
 }
 
-// Initial check
-updateAsideDisplay();
+mobileMenu();
 
-window.addEventListener('resize', updateAsideDisplay);
-
-// Display/hide aside menu
-displayMenu.addEventListener('click', () => {
-  aside.style.display = "block";
-});
-
-hideMenu.addEventListener('click', () => {
-  aside.style.display = "none";
-});
+window.addEventListener('resize', mobileMenu);
